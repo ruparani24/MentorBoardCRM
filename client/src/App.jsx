b8b1381f3af00d74f982b9013b21ad2547326497
G.S.Ruparani <ranicrowns@gmail.com>
2025-08-23 09:08:13 +0530

    q

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,37 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Dashboard from "./pages/Dashboard";
 import Mentors from "./pages/Mentors";
 import Mentees from "./pages/Mentees";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <Routes>
-      {/* Public route */}
+      {/* Public routes */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
-      {/* Protected routes under Layout */}
-      <Route path="/" element={<Layout />}>
+      {/* Protected routes */}
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <Layout />
+          </PrivateRoute>
+        }
+      >
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="mentors" element={<Mentors />} />
         <Route path="mentees" element={<Mentees />} />
+        {/* Redirect default "/" to dashboard */}
+        <Route index element={<Navigate to="dashboard" replace />} />
       </Route>
+
+      {/* Catch all - redirect unknown routes to login */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
